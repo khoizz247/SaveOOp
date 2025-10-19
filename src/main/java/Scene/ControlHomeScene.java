5abee05d757b9f7aaa571088fe844d04997f97d1
phatpham-24020265 <24020265@vnu.edu.vn>
2025-10-19 16:27:49 +0700

    Thêm menu

--- src/main/java/Scene/ControlHomeScene.java
+++ src/main/java/Scene/ControlHomeScene.java
@@ -1,14 +1,38 @@
 package Scene;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class ControlHomeScene {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button startButton;
+
+    @FXML
+    public void initialize() {
+        Platform.runLater(() -> {
+            if (startButton != null && startButton.getParent() != null) {
+                startButton.getParent().requestFocus();
+            }
+        });
+    }
+
+    @FXML
+    private void startGame(ActionEvent event) throws Exception {
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/myarkanoid/ongame-view.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setTitle("My Game");
+    }
+
+    @FXML
+    private void quitGame(ActionEvent event) {
+        Platform.exit();
     }
 }
